4f090a227acb8b6bbd78aea1d220213ffba2acff
incogNic0 <incognico.dev@gmail.com>
2024-10-10 09:20:06 -0500

    initial config setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsChecker({ typescript: true })],
+    plugins: [react(), tsChecker({ typescript: true })],
+    server: {
+        port: 3000,
+    },
 });
