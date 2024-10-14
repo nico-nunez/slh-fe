4bd24db5875ba448bafc9198b4515aa21176d547
incogNic0 <incognico.dev@gmail.com>
2024-10-14 16:07:43 -0500

    add basic blue pallette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		fontFamily: {
-			sans: ['Inter', 'sans-serif'],
-		},
-		extend: {},
-	},
-	plugins: ['prettier-plugin-tailwindcss'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                'winter-blue-100': '#86feff',
+                'winter-blue-200': '#8de2ff',
+                'winter-blue-300': '#c3f9ff',
+                'winter-blue-400': '#00d9e6',
+                'winter-blue-500': '#00e4ff',
+            },
+        },
+    },
+    plugins: ['prettier-plugin-tailwindcss'],
 };
