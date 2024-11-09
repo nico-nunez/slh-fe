d9c88448c10e08ea9a714ab2cba8fd37ce80e745
incogNic0 <incognico.dev@gmail.com>
2024-11-09 09:39:53 -0600

    update tschecker

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsChecker({ typescript: true }), TanStackRouterVite()],
+    plugins: [tsChecker({ typescript: true }), react(), TanStackRouterVite()],
     server: {
         port: 3000,
     },
